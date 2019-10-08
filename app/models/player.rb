2dbb18b3360804fd5e6debc1357e2964732821d4
Kyle Specht <habitatt11@gmail.com>
2019-10-07 19:10:28 -0500

    Added validations to Team and Player Models

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,2 +1,11 @@
+# frozen_string_literal: true
+
 class Player < ApplicationRecord
+  belongs_to :team
+
+  validates :teamid, :jerseynumber, :positiontype, :positionname, :fullname,
+            :playerid, presence: true
+  validates :teamid, :jerseynumber, :playerid,
+            numericality: { only_integer: true }
+  validates :teamid, :playerid, uniqueness: true
 end
--- app/models/team.rb
+++ app/models/team.rb
@@ -1,2 +1,11 @@
+# frozen_string_literal: true
+
 class Team < ApplicationRecord
+  has_many :players
+
+  validates :teamid, :name, :venue, :city, :division, :divisionid, :confrence,
+            :confrenceid, :year, presence: true
+  validates :teamid, :divisionid, :confrenceid, :year,
+            numericality: { only_integer: true }
+  validates :teamid, uniqueness: true
 end
