6ab737bdc7c4918e7ada0439170248c2ea0ee159
Kyle Specht <habitatt11@gmail.com>
2019-10-21 12:29:52 -0500

    Added Pages controller + static pages Home + About

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,21 +2,21 @@
 
 class PagesController < ApplicationController
   # GET /static/:permalink
-  # def permalink
-  #   @page = Page.find_by_permalink(params[:permalink])
+  def permalink
+    @page = Page.find_by_permalink(params[:permalink])
 
-  #   if @page
-  #     render :show # /app/views/pages/show.html.erb
-  #   else
-  #     redirect_to root_path
-  #   end
-  # end
+    if @page
+      render :show # /app/views/pages/show.html.erb
+    else
+      redirect_to root_path
+    end
+  end
 
   # GET /pages
   # GET /pages.json
-  # def index
-  #   @pages = Page.all
-  # end
+  def index
+    @pages = Page.all
+  end
 
   def show
     render template: "pages/#{params[:page]}"
